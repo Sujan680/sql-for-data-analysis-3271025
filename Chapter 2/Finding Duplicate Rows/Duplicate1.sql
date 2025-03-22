5420768d0a86cee38260317518de18cbd7621090
Sujan Budhathoki <75134082+Sujan680@users.noreply.github.com>
2025-03-22 04:40:08 +0000

    Finding duplicates data with help of Group by and Having statement with aggregate functions.

--- Chapter 2/Finding Duplicate Rows/Duplicate1.sql
+++ Chapter 2/Finding Duplicate Rows/Duplicate1.sql
@@ -1,10 +1,12 @@
-SELECT C.CustomerID,
+SELECT 
+  C.CustomerID,
   C.FirstName,
   C.LastName,
   COUNT(1) as NUM_ORDERS
 FROM Customer C
   JOIN Orders O ON C.CustomerID = O.CustomerID
-GROUP BY C.CustomerID,
+GROUP BY 
+  C.CustomerID,
   C.FirstName,
   C.LastName
 ORDER BY NUM_ORDERS DESC;
